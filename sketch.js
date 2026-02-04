aecca555a82accfefda1f7f710ffb94398595eac
Mridula Manoj <73586191+mridula13@users.noreply.github.com>
2026-02-05 00:56:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,128 +1,117 @@
 var database;
 
-var dog, sadDog, happyDog;
-var foodS, foodStock;
-var fedTime, lastFed;
-var feed, addFood;
+var dog, happyDog;
+var foodStock, foodS;
+var feedTime, lastFed;
+var feedBtn, addFoodBtn;
 var foodObj;
 var milkImg, milk;
-var nameI;    
-var nameRef;  
-var Name = "";
+var nameInput, nameRef;
+var petName = "";
 
 function preload() {
   dogImg = loadImage("Dog.png");
-  milkImg = loadImage("Milk.png");
   happyDogImg = loadImage("happy_dog.png");
+  milkImg = loadImage("Milk.png");
 }
 
-
 function setup() {
-  createCanvas(1000,400);
+  createCanvas(1000, 400);
 
   database = firebase.database();
   foodObj = new Food();
 
-  foodStock = database.ref('Food');
+  foodStock = database.ref("Food");
   foodStock.on("value", readStock);
 
-  fedTime = database.ref('FeedTime');
-  fedTime.on("value", function(data){
+  feedTime = database.ref("FeedTime");
+  feedTime.on("value", (data) => {
     lastFed = data.val();
   });
 
-  dog = createSprite(800, 200, 150, 150);
+  dog = createSprite(800, 200);
   dog.addImage(dogImg);
   dog.scale = 0.15;
 
-  nameI = createInput("Enter Name");
-  nameI.position(230, 95);
+  nameInput = createInput("Enter Name");
+  nameInput.position(230, 95);
 
   nameRef = database.ref("Name");
-
-  nameRef.on("value", function(data) {
-    Name = data.val();
-    if (Name) {
-      nameI.value(Name);
-    }
+  nameRef.on("value", (data) => {
+    petName = data.val();
+    if (petName) nameInput.value(petName);
   });
 
-  let saveName = createButton("Save Name");
-  saveName.position(230, 120);
-  saveName.mousePressed(() => {
-    nameRef.set(nameI.value());
+  let saveNameBtn = createButton("Save Name");
+  saveNameBtn.position(230, 125);
+  saveNameBtn.mousePressed(() => {
+    nameRef.set(nameInput.value());
   });
 
-  feed = createButton("Feed the dog");
-  feed.position(700, 95);
-  feed.mousePressed(feedDog);
+  // Buttons
+  feedBtn = createButton("Feed the dog");
+  feedBtn.position(700, 95);
+  feedBtn.mousePressed(feedDog);
 
-  addFood = createButton("Add Food");
-  addFood.position(800, 95);
-  addFood.mousePressed(addFoods);
+  addFoodBtn = createButton("Add Food");
+  addFoodBtn.position(820, 95);
+  addFoodBtn.mousePressed(addFood);
 }
 
-
 function draw() {
-  background(46,139,87);
+  background(46, 139, 87);
 
   foodObj.display();
 
-  if (Name) {
+  // Pet name
+  if (petName) {
     fill(255);
     textSize(18);
     textAlign(CENTER);
-
-    text(Name, dog.position.x, dog.position.y + 100);
+    text(petName, dog.position.x, dog.position.y + 100);
   }
-
 
   fill(255);
   textSize(15);
+  textAlign(LEFT);
 
-  if (lastFed >= 12) {
-    text("Last Feed : " + (lastFed % 12 || 12) + " PM", 350, 30);
-  } else {
-    text("Last Feed : " + lastFed + " AM", 350, 30);
+  if (lastFed !== undefined) {
+    let time = lastFed % 12 || 12;
+    let ampm = lastFed >= 12 ? "PM" : "AM";
+    text("Last Feed : " + time + " " + ampm, 350, 30);
   }
 
   drawSprites();
 }
 
-
-function readStock(data)
-{
+function readStock(data) {
   foodS = data.val();
   foodObj.updateFoodStock(foodS);
 }
 
-function feedDog()
-{
+function feedDog() {
+  if (foodObj.getFoodStock() <= 0) return;
+
   dog.addImage(happyDogImg);
 
-  milk = createSprite( 720, 220, 10, 10);
+  if (milk) milk.remove();
+
+  milk = createSprite(720, 220);
   milk.addImage(milkImg);
   milk.scale = 0.1;
 
-  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  foodObj.updateFoodStock(foodObj.getFoodStock() - 1);
 
-  database.ref('/').update({
-    
+  database.ref("/").update({
     Food: foodObj.getFoodStock(),
-
     FeedTime: hour()
-  })
+  });
 }
 
-function addFoods()
-{
-  foodS = foodObj.getFoodStock() + 1;
-  database.ref('/').update({
-    Food:foodS
-  })
+function addFood() {
+  let currentFood = foodObj.getFoodStock() || 0;
 
+  database.ref("/").update({
+    Food: currentFood + 1
+  });
 }
-
-
-
-
